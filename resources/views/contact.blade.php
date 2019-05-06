4ed2207065787fb372aa0f6346979d9d0de99a02
sunzid02 <sunzid02@gmail.com>
2019-05-06 13:06:29 +0600

    creating first route

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,13 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Here We go</h1>
+    <h1>Contact From</h1>
 
-    <p>
-        <a href="/contact">Contact</a> us to learn more
-    </p>
+    
 </body>
 </html>
