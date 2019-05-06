8d23b13c03190d554192126202e238d9127f5bd4
sunzid02 <sunzid02@gmail.com>
2019-05-06 13:59:29 +0600

    send data from route to view

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,15 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Laracasts')</title>
 </head>
-<body>
+    <body>
 
-    
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About Us</a></li>
-        <li><a href="/contact">Contact</a></li>
-    </ul>
+        
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About Us</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
 
-    @yield('content')
-</body>
+        @yield('content')
+    </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,17 @@
 @endsection
 
 @section('content')
-    <h1>My first Website</h1>
+
+    <h1>My {{ $foo }} Website</h1>
+
+    <ul>
+
+        @foreach( $tasks as $task )
+
+             <li> {{ $task }} </li>
+
+        @endforeach
+
+    </ul>
+
 @endsection
