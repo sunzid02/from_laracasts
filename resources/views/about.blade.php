c274db799cc30564c84a077bdc4a8ca40e693781
sunzid02 <sunzid02@gmail.com>
2019-05-06 13:34:03 +0600

    yield added

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+
+@section('title')
+    About
+@endsection
+
+@section('content')
+    <h1>About us</h1>
+
+    <p>
+        asbjkdbasjkd ajksdkjashdjk asasjkdhaskjdh asdjkhaskjdhasjkdhkashd
+        asbjkdbasjkd ajksdkjashdjk asasjkdhaskjdh asdjkhaskjdhasjkdhkashd
+        asbjkdbasjkd ajksdkjashdjk asasjkdhaskjdh asdjkhaskjdhasjkdhkashd
+        asbjkdbasjkd ajksdkjashdjk asasjkdhaskjdh asdjkhaskjdhasjkdhkashd
+        asbjkdbasjkd ajksdkjashdjk asasjkdhaskjdh asdjkhaskjdhasjkdhkashd
+        asbjkdbasjkd ajksdkjashdjk asasjkdhaskjdh asdjkhaskjdhasjkdhkashd
+        asbjkdbasjkd ajksdkjashdjk asasjkdhaskjdh asdjkhaskjdhasjkdhkashd
+    </p>
+@endsection
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Contact From</h1>
+@extends('layout')
 
-    
-</body>
-</html>
+@section('title')
+    Contact Us
+@endsection
+
+@section('content')
+    <h1>Contact Form</h1>
+@endsection
