b37c7365c71a1a55814192ab0ca473a3c9077cb1
sunzid02 <sunzid02@gmail.com>
2019-05-07 15:01:42 +0600

    new route with resource added

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,14 @@ Route::get('/contact', 'PagesController@contact');
 
 Route::get('/about', 'PagesController@about');
 
-Route::get('/projects', 'ProjectController@index');
-Route::post('/projects', 'ProjectController@store');
+//Projects 
+// Route::get('/projects', 'ProjectController@index');
+// Route::post('/projects', 'ProjectController@store');
 
-Route::get('/projects/create', 'ProjectController@create');
+// Route::get('/projects/create', 'ProjectController@create');
+
+
+Route::resource('projects', 'ProjectController');
+
 
 
