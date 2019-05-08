df9db5d50d76545bcf475d683dce32daafe644f7
sunzid02 <sunzid02@gmail.com>
2019-05-08 11:57:25 +0600

    patching new method

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,20 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Laracasts')</title>
-</head>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Title Page</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.0/css/bulma.css"/>
+    </head>
     <body>
-
-        
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About Us</a></li>
-            <li><a href="/contact">Contact</a></li>
-        </ul>
-
-        @yield('content')
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
